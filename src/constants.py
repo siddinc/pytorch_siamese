9fcfc60f08f03ed04fe89cae3643c5c1eeb1b203
siddinc <sanghavisidd@gmail.com>
2021-02-19 21:08:08 +0530

    define network architecture

---
+++ src/constants.py
@@ -0,0 +1,32 @@
+import os
+
+
+DATASET_PATH = os.path.abspath("../dataset")
+MPL_PATH = os.path.abspath("../fig.mplstyle")
+
+INPUT_SHAPE = (3000, 1)
+N_TIMESTEPS = 3000
+N_FEATURES = 1
+CHANNEL_INDEX = 22    #22, 55, 63
+VAL_SPLIT = 0.3
+TEST_SPLIT = 0.3
+
+L1_PENALTY = 0.001
+L2_PENALTY = 0.001
+ALPHA = 0.2
+MARGIN = 1.0
+
+BATCH_SIZE = 16
+N_EPOCHS = 20
+INIT_LR = 0.0003
+AMS_GRAD = True
+BETA_1 = 0.5
+BETA_2 = 0.999
+
+N_TRIALS = 500
+SUPPORT_SET_SIZE = 20
+
+NORM_DEG = {
+  "manhattan": 1,
+  "euclidean": 2,
+}
---
+++ src/network.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchsummary import summary
+
+from constants import (
+  BATCH_SIZE,
+)
+
+
+class SiameseNet(nn.Module):
+
+  def __init__(self):
+    super(SiameseNet, self).__init__()
+    self.cnn = nn.Sequential(
+      self.conv1d_block(1, 16, 3, stride=1, padding=1),  #3000x16
+      nn.MaxPool1d(2),  #1500x16
+      self.conv1d_block(16, 32, 3, 1, 1),  #1500x32
+      nn.MaxPool1d(2),  #750x32
+      self.conv1d_block(32, 64, 3, 1, 1),  #750x64
+      nn.MaxPool1d(2),  #375x64
+      self.conv1d_block(64, 128, 3, 1, 1), #375x128
+      nn.AvgPool1d(375), #1, 128
+      nn.Sigmoid(),
+    )
+
+    self.sigmoid = nn.Sigmoid()
+
+  def conv1d_block(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+    return nn.Sequential(
+      nn.Conv1d(in_channels, out_channels, kernel_size, stride=stride, padding=padding),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.BatchNorm1d(out_channels),
+    )
+  
+  def forward_once(self, x):
+    output = self.cnn(x)
+    output = output.view(output.size()[0], -1)
+    return output
+  
+  def forward(self, input1, input2):
+    embedding1 = self.forward_once(input1)
+    embedding2 = self.forward_once(input2)
+    distance = nn.PairwiseDistance(p=2)(embedding1, embedding2)
+    final_output = self.sigmoid(distance)
+    return final_output
+
+if __name__ == "__main__":
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  net = SiameseNet().to(device)
+  summary(net, [(1, 3000), (1, 3000)], batch_size=16)
